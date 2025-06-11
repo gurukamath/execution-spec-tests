962737442dfd75181d37af0c86c85b2a0777bd7e
Guruprasad Kamath <guru241987@gmail.com>
2025-06-11 15:38:06 +0200

    remove tests

--- tests/frontier/scenarios/scenarios/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Scenarios common import."""
--- tests/unscheduled/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Test cases for unscheduled EVM functionality. A temporary home for features that are not yet CFI'd for inclusion in the next hardfork."""  # noqa: E501
